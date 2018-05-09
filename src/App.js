ee94bdc916acdc82d6fb1e1a57221a63918f7a0c
Jake Knowles <jakeyknowles@hotmail.co.uk>
2018-05-09 16:51:41 +0100

    Section 3 basics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Person from './person/person';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   //Props is to share data to a another component - state is to update the component data within the component
+
+  //Containers have all the handlers and methods in it which is then passed via props to its children components
 
   //state only available in components extending Component
   state = {
@@ -14,12 +15,48 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = (newName) => {
+    // DO NOT CHANGE THE STATE LIKE THIS: this.state.persons[0].name = "Jake is a twat";
+    this.setState({persons: [
+      {name: newName, age: 24},
+      {name: "Steve", age: 55}
+    ]})
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({persons: [
+      {name: event.target.value, age: 24},
+      {name: "Steve", age: 55}
+    ]})
+  }
+
+  //click attr is passing a clickhandler to change the data to the parent component
+  //IE: onClick={this.switchNameHandler.bind(this, "test")}
+
   render() {
+    //This is how to inline style - use files, it's easier and better - only positive is its only scoped for the component you are using
+    
+    const style = {
+      backgroundColour: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
+
     return (
       <div className="App">
         <h1>This is my app</h1>
-        <button>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <button 
+        style={style}
+        onClick={this.switchNameHandler.bind(this, "Jake is a twat")}
+        >Switch Name</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}
+        click={this.switchNameHandler.bind(this, "Jake is not a twat")}
+        changed={this.nameChangedHandler}
+        />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
       </div>
     );
--- src/person/person.js
+++ src/person/person.js
@@ -1,21 +1,28 @@
 import React from 'react';
+import './person.css';
+//This is a way of envoking a component with out a class - functional component - want to use this as much as possible to just render dynamic content
 
-//This is a way of envoking a component with out a class - functional component
-
-// const person = (props) => {
-//   return <p>I'm a person called {props.name} and I am {props.age}</p>
-// }
-
-//This is a class-based component - sometimes not needed for small simple components
-class Person extends React.Component {
-  render() {
-    return (
-      <div>
-        <p>I'm a person called {this.props.name} and I am {this.props.age}</p>
-        <p>{this.props.children}</p>
-      </div>
-    )
-  }
+const Person = (props) => {
+  return (
+    <div className="person">
+      <p onClick={props.click}>I'm a person called {props.name} and I am {props.age}</p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  )
 }
+
+//This is a class-based component - sometimes not needed for small simple components - this is used to change state in a component as well as rendering
+
+// class Person extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <p>I'm a person called {this.props.name} and I am {this.props.age}</p>
+//         <p>{this.props.children}</p>
+//       </div>
+//     )
+//   }
+// }
 
 export default Person;
