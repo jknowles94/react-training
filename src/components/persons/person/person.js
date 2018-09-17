27a3341b0b74d562e4b684f4d27b88eae1063ef1
Jake Knowles <jakeyknowles@hotmail.co.uk>
2018-09-17 18:56:34 +0100

    change persons and person to be stateful components

--- src/components/persons/person/person.js
+++ src/components/persons/person/person.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './person.css';
 //This is a way of envoking a component with out a class - functional component - want to use this as much as possible to just render dynamic content
 
-const Person = (props) => {
-  return (
-    <div className="person">
-      <p onClick={props.click}>I'm a person called {props.name} and I am {props.age}</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  )
+class Person extends Component {
+	render() {
+		return (
+		  <div className="person">
+		    <p onClick={this.props.click}>Im a person called {this.props.name} and I am {this.props.age}</p>
+		    <p>{this.props.children}</p>
+		    <input type="text" onChange={this.props.changed} value={this.props.name} />
+		  </div>
+		)
+	}
 }
 
 //This is a class-based component - sometimes not needed for small simple components - this is used to change state in a component as well as rendering
--- src/components/persons/persons.js
+++ src/components/persons/persons.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Person from './person/person';
 
 //ONLY USE FUNCTIONAL COMPONENTS WHEN YOU DONT NEED TO MANAGE STATE AND LIFECYCLE HOOKS - THESE SHOULD BE PASSED DOWN FROM THE CONTAINER COMPONENT(STATEFUL) VIA PROPS
 
 //This splits out the app.js (container component) to make for more readable code and good practice
 
-const Persons = (props) => props.persons.map((person, index) => {
-    return (
-      <Person key={person.id}
-       name={person.name} 
-       age={person.age} 
-       click={() => props.clicked(index)} 
-       changed={(event) => props.changed(person.id, event)} />
-    )
-});
+class Persons extends Component {
+	render() {
+		return this.props.persons.map((person, index) => {
+			return (
+	      <Person key={person.id}
+	       name={person.name} 
+	       age={person.age} 
+	       click={() => this.props.clicked(index)} 
+	       changed={(event) => this.props.changed(person.id, event)} />
+	    )
+		});
+	}
+}
 
 export default Persons;
